69c629d7323a4c394db4ead118a820635c7f1b49
Yash Kataruka <yashkataruka08@gmail.com>
2019-12-06 23:20:02 +0530

    06-12-2019

---
+++ 06-12-2019/Task1/Strings_Task1.py
@@ -0,0 +1,22 @@
+#printing a basic string using one quote
+a = 'Hi, My name is Yash Kataruka 1'
+print(a)
+print('\n')
+
+#printing a basic string using two quotes
+b = "Hi, My name is Yash Kataruka 2"
+print(b)
+
+#printing a set of hindi characters
+c = 'हिंदी समाचार - लाइव हिन्दी न्यूज़'
+print(c)
+
+#printing the apostrophe character
+e = 'I didn\'t do the reasoning exercise'
+print(e)
+
+#printing a paragraph using three single quotes
+f ='''Lorem ipsum dolor sit amet, consectetur adipisi
+cing elit, sed do eiusmod tempor incididunt ut labore
+ et dolore magna aliqua. Ut enim ad minim venia'''
+print(f)
